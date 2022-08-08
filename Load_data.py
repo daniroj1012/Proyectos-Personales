c3cb65d41aa68c3c11469c6de20664ecbba73b42
Daniel Rojas <daniel_rojas@hakkoda.io>
2022-08-08 15:08:04 -0600

    Test Connection

--- Load_data.py
+++ Load_data.py
@@ -2,8 +2,11 @@ import pandas as pd
 import snowflake.connector
 from snowflake.connector.pandas_tools import write_pandas
 import os
+import pyarrow
 from os.path import join, dirname
 from dotenv import load_dotenv
+
+os.chdir('/Users/danielrojaschacon/Documents')
 dotenv_path = join(dirname('snowflake'),'snowflake.env')
 
 
@@ -18,13 +21,14 @@ SF_PASSWORD   = os.getenv('SF_PASSWORD')
 SF_ROLE   = os.getenv('SF_ROLE')
 
 
-df= pd.read_csv("Diplomas.csv")  #LOAD FILE INTO DATA FRAME
-df.info()
-df 
+
+# df= pd.read_csv("Diplomas.csv")  #LOAD FILE INTO DATA FRAME
+# df.info()
+# df 
 
 
-df_cor=df.assign(YEAR=df["AÑO"])
-df_cor=df_cor[["YEAR","SECTOR_UNIVERSITARIO","UNIVERSIDAD","SEDE_CONARE","TIPO_SEDE","REGION_PLANIFICACION_SEDE","CARRERA","GRADO_ACADEMICO","NIVEL_ACADEMICO","AREA_CONOCIMIENTO","STEM_MICITT","SEXO","EDAD","PROVINCIA_GRADUADO","CANTON_GRADUADO","DISTRITO_GRADUADO","TIPO_NACIONALIDAD"]] #FILTER
+# df_cor=df.assign(YEAR=df["AÑO"])
+# df_cor=df_cor[["YEAR","SECTOR_UNIVERSITARIO","UNIVERSIDAD","SEDE_CONARE","TIPO_SEDE","REGION_PLANIFICACION_SEDE","CARRERA","GRADO_ACADEMICO","NIVEL_ACADEMICO","AREA_CONOCIMIENTO","STEM_MICITT","SEXO","EDAD","PROVINCIA_GRADUADO","CANTON_GRADUADO","DISTRITO_GRADUADO","TIPO_NACIONALIDAD"]] #FILTER
 #df_cor.info()
 #df_cor
 #df_cor = df_cor.drop(df_cor[df_cor.PROVINCIA_GRADUADO =='Sin Información'].index) 
@@ -35,6 +39,6 @@ df_cor=df_cor[["YEAR","SECTOR_UNIVERSITARIO","UNIVERSIDAD","SEDE_CONARE","TIPO_S
 
 ##df_cor = df_cor.drop(df_cor[df_cor.DISTRITO_GRADUADO =='Sin Información'].index) 
 ##df_cor = df_cor.drop(df_cor[df_cor.DISTRITO_GRADUADO =='Sin información'].index) 
-print('----------------------------------------------------------------------------------')
-df_cor.info()
-df_cor       
+# print('----------------------------------------------------------------------------------')
+# df_cor.info()
+# df_cor       
