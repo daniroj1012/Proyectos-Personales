cfc71b2e6bb3a7f65194cbcc5085b29a5f759a28
Daniel Rojas <daniel_rojas@hakkoda.io>
2022-08-09 19:07:35 -0600

    app development

--- data_analysis_app.py
+++ data_analysis_app.py
@@ -1,10 +1,41 @@
 import sys
-import cachetools
 import os
 import pandas as pd
 import streamlit as st
-import joblib
 import matplotlib.pyplot as plt
 import operator as op
 from itertools import chain
 from PIL import Image
+import snowflake.connector
+
+
+image_s = Image.open('logo_s.png')
+image = Image.open('logo.png')
+#st.set_option('deprecation.showPyplotGlobalUse', False)
+st.set_page_config(
+     page_title="Hakkoda data analysis tool",
+     page_icon=image_s,
+     layout="wide",
+     initial_sidebar_state="expanded",
+     menu_items={
+         'Get Help': 'https://hakkoda.io/contact/',
+         'About': "This is an app powered by Hakkoda for Data Analysis in Python and Streamlit"
+     }
+)
+
+def get_data():
+    return df
+
+def show_data():
+    col1, col2, col3= st.columns(3)
+    with col2:
+        st.image(image, caption='Empowering data-driven organizations', width=400)
+    st.title("Diploma Data Analysis",)
+    st.caption("Powered by Hakkoda for Data Analysis with Python | Made with Streamlit")
+    st.write('El objetivo de esta demo es mostrar una conexión entre snowflake y una aplicación para analisis de datos hecha en python con la libreria streamlit')
+
+
+
+
+if __name__=="__main__":
+    show_data()
