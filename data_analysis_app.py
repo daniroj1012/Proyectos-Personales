044696aa56b77a366a0d7ba3f066b1c7d04d67bc
Daniel Rojas <daniel_rojas@hakkoda.io>
2022-08-16 21:01:48 -0600

    data frame show

--- data_analysis_app.py
+++ data_analysis_app.py
@@ -9,6 +9,7 @@ from PIL import Image
 import snowflake.connector
 
 
+
 image_s = Image.open('logo_s.png')
 image = Image.open('logo.png')
 #st.set_option('deprecation.showPyplotGlobalUse', False)
@@ -23,8 +24,22 @@ st.set_page_config(
      }
 )
 
-def get_data():
-    return df
+# Initialize connection.
+# Uses st.experimental_singleton to only run once.
+@st.experimental_singleton
+def init_connection():
+    return snowflake.connector.connect(**st.secrets["snowflake"])
+
+conn = init_connection()
+
+# Perform query.
+# Uses st.experimental_memo to only rerun when the query changes or after 10 min.
+@st.experimental_memo(ttl=600)
+def run_query(query):
+    with conn.cursor() as cur:
+        cur.execute(query)
+        return cur.fetchall()
+
 
 def show_data():
     col1, col2, col3= st.columns(3)
@@ -33,6 +48,18 @@ def show_data():
     st.title("Diploma Data Analysis",)
     st.caption("Powered by Hakkoda for Data Analysis with Python | Made with Streamlit")
     st.write('El objetivo de esta demo es mostrar una conexión entre snowflake y una aplicación para analisis de datos hecha en python con la libreria streamlit')
+
+    st.subheader('Datos de Matriculados')
+    matriculados = pd.DataFrame(run_query("SELECT * FROM MATRICULADOS;"))
+    st.dataframe(matriculados)
+    st.subheader('Datos de Egresados')
+    egresados= pd.DataFrame(run_query("SELECT * FROM EGRESADOS;"))
+    st.dataframe(egresados)
+    
+    
+   
+
+    
 
 
 
